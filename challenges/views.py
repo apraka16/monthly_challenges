463eaa0a0a0afda82f4719f3e9b11b46e37d923a
Abhinav Prakash <abhinavprakash@Admins-MacBook-Pro-85.local>
2023-01-26 12:59:25 +0530

    First code

---
+++ challenges/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month",
+    "february": "Walk for at least 20 mins per day",
+    "march": "Learn Django for at least 20 mins per day",
+    "april": "Eat more greens each day",
+    "may": "Walk for at least 20 mins per day",
+    "june": "Summber vacations in this month",
+    "july": "Learn Django for at least 20 mins per day",
+    "august": "Eat more greens each day",
+    "september": "Learn Django for at least 20 mins per day",
+    "october": "Eat more greens each day",
+    "november": "Walk for at least 20 mins per day",
+    "december": "End of year. No task.",
+}
+
+# Create your views here.
+
+
+def monthly_challenges_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse(
+        "month-challenge", args=[redirect_month])  # /challenge/january
+
+    return HttpResponseRedirect(redirect_path)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported")
